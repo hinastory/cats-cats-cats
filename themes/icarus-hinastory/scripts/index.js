201e6be156fc55badf4446daf75068d9f8525ff4
hinastory <1696779+hinastory@users.noreply.github.com>
2019-03-09 02:47:01 +0900

    modify post

--- themes/icarus-hinastory/scripts/index.js
+++ themes/icarus-hinastory/scripts/index.js
@@ -13,7 +13,6 @@ require('../includes/helpers/override')(hexo);
 require('../includes/helpers/page')(hexo);
 require('../includes/helpers/site')(hexo);
 require('../includes/tags/elink')(hexo);
-require('../includes/tags/details')(hexo);
 
 // Debug helper
 hexo.extend.helper.register('console', function () {
